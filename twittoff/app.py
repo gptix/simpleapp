db5c9df82ef2d40ec6a4f819011be4ddc0f0da6c
Jud Taylor <jud.taylor@protonmail.com>
2020-03-22 13:18:24 -0400

    Removed more comments

--- twittoff/app.py
+++ twittoff/app.py
@@ -1,10 +1,4 @@
-# from decouple import config
 from flask import Flask, render_template, request
-
-# from .models import DB, User
-# from .predict import predict_user
-# from .twitter import add_or_update_user, update_all_users
-
 
 def create_app():
     """Create and configure an instance of the Flask application"""
